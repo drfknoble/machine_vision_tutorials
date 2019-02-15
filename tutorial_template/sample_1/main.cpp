35a60ad879de96e4d9998f93febcd6040492a3dd
drfrazernoble <f.k.noble@massey.ac.nz>
2019-02-15 14:27:15 +1300

    Removed template.

--- tutorial_template/sample_1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "main.hpp"
-
-int main(int argc, char* argv) {
-
-    
-    return 0;
-}
--- tutorial_template/sample_1/main.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __MAIN_HPP__
-#define __MAIN_HPP__
-
-#include <opencv2/opencv.hpp>
-
-#endif //__MAIN_HPP__

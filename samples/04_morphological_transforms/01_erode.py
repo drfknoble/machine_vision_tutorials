1bfbaf14b071f8d3011c2d92303dee6fcf7e8dd6
drfrazernoble <f.k.noble@massey.ac.nz>
2020-05-20 09:17:15 +1200

    Added a morphological transform sample

---
+++ samples/04_morphological_transforms/01_erode.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import numpy as np
+import random
+
+def main():
+
+    # Create image
+
+    rows, cols = 480, 640
+    img = np.zeros((rows, cols), dtype=np.uint8)
+
+    # Draw circles at the centre
+
+    cv.circle(img, (320, 240), 150, (255), 40)
+    cv.circle(img, (320, 240), 100, (255), 40)
+
+    # Generate random noise
+
+    coordinates = [(int(random.random() * cols), int(random.random() * rows)) for _ in range(100)]
+
+    for c in coordinates:
+
+        cv.circle(img, c, 2, (255), -1)
+
+    cv.imshow("img", img)
+    cv.waitKey(0)
+    cv.imwrite("data/morph_img.png", img)
+
+    # Erode image using different shapes
+
+    shapes = [cv.MORPH_RECT, cv.MORPH_CROSS, cv.MORPH_ELLIPSE]
+    shapes_label = ["MORPH_RECT", "MORPH_CROSS", "MORPH_ELLIPSE"]
+
+    for i, s in enumerate(shapes):
+
+        kernel = cv.getStructuringElement(s, (20, 20))
+        print("{}: \n{}\n".format(shapes_label[i], kernel))
+
+        eroded = cv.erode(img, kernel)
+
+        cv.imshow("Eroded + {}".format(shapes_label[i]), eroded)
+        cv.waitKey(0)        
+        cv.imwrite("data/eroded_{}.png".format(shapes_label[i]), eroded)
+   
+    cv.destroyAllWindows()
+
+    return
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ samples/04_morphological_transforms/04_closing.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import numpy as np
+import random
+
+def main():
+
+    # Create image
+
+    rows, cols = 480, 640
+    img = np.zeros((rows, cols), dtype=np.uint8)
+
+    # Draw circles at the centre
+
+    cv.circle(img, (320, 240), 150, (255), 40)
+    cv.circle(img, (320, 240), 100, (255), 40)
+
+    # Generate random noise
+
+    coordinates = [(int(random.random() * cols), int(random.random() * rows)) for _ in range(100)]
+
+    for c in coordinates:
+
+        cv.circle(img, c, 2, (255), -1)
+
+    cv.imshow("img", img)
+    cv.waitKey(0)
+    cv.imwrite("data/morph_img.png", img)
+  
+    # Closing
+
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (20, 20))
+
+    # closed = cv.dilate(img, kernel)
+    # closed = cv.erode(opened, kernel)
+
+    closed = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+
+    cv.imshow("Closing", closed)
+    cv.waitKey(0)
+    cv.imwrite("data/closed.png", closed)
+    
+    cv.destroyAllWindows()
+
+    return
+
+
+if __name__ == "__main__":
+
+    main()
